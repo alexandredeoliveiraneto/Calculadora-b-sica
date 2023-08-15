6f02a28876cda95d70444b8411199a91b2bbdb40
alexandre oliveira <alexandreoliveira.ge@yahoo.com>
2023-08-15 09:32:16 -0300

    Adicionando funcionalidade de divisão

--- script.js
+++ script.js
@@ -33,7 +33,27 @@ function multiplicar() {
     const resultado = document.getElementById('resmulti');
     resultado.innerHTML = `O resultado da multiplicação entre ${n1Multi} e ${n2Multi} é igual a ${n1Multi * n2Multi} !`;
 
-    }
+}
 
-    const btnMulti = document.getElementById('botaomulti');
-    btnMulti.addEventListener('click', multiplicar);
+const btnMulti = document.getElementById('botaomulti');
+btnMulti.addEventListener('click', multiplicar);
+
+function dividir() {
+
+    btnDiv.style.background = 'green';
+
+    const txtN1div = document.getElementById('n1div');
+    const n1Div = Number(txtN1div.value);
+
+    const txtN2Div = document.getElementById('n2div');
+    const n2Div = Number(txtN2Div.value);
+
+    const resultado = document.getElementById('resdiv');
+    resultado.innerHTML = `O resultado da divisão entre ${n1Div} e ${n2Div} é igual a ${n1Div / n2Div} !`;
+
+}
+
+const btnDiv = document.getElementById('botaodiv');
+btnDiv.addEventListener('click', dividir);
+
+
